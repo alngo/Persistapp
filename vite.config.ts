e50dc9011815866b11da6d27cff865a26be5f042
alngo <alxengo.pro@gmail.com>
2023-11-25 18:59:18 +0100

    Implement subroute

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), topLevelAwait()],
+	worker: {
+		plugins: [topLevelAwait()]
+	},
 	test: {
 		globals: true,
 		environment: 'jsdom',
