4e4e467f2bdbc2b10498c9ba11867310836604ac
alngo <alxengo.pro@gmail.com>
2023-11-26 15:01:44 +0100

    Add sessionStorage route

---
+++ src/routes/sessionstorage/+page.svelte
@@ -0,0 +1,3 @@
+<h1>SessionStorage</h1>
+<a href="sessionstorage/sub">Sub Route</a>
+<a href="/">root</a>
---
+++ src/routes/sessionstorage/sub/+page.svelte
@@ -0,0 +1,3 @@
+<h1>SessionStorage/sub</h1>
+<a href="/sessionstorage">sessionstorage</a>
+<a href="/">root</a>
