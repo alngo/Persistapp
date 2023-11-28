405fdc0e69cfd1165310f2c2f364bc0cd574791c
alngo <alxengo.pro@gmail.com>
2023-11-28 12:56:32 +0100

    idb

--- src/lib/storage/idbStorage/idbStorageBox.test.ts
+++ src/lib/storage/idbStorage/idbStorageBox.test.ts
@@ -1,7 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import * as _ from 'fake-indexeddb/auto';
 import idbStorageBox from './idbStorageBox';
-import { openIdb } from './utils.ts';
 
 describe('idbStorageBox', async () => {
 	it('should be able to add and get a value', () => {
